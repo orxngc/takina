d1c8549421ba2c5064b0e7d7e8ce89c85decc52a
orxngc <orangc@proton.me>
2024-10-10 18:47:36 +0300

    refactor(util): remindme error embeds were not red

--- takina/cogs/util/remindme.py
+++ takina/cogs/util/remindme.py
@@ -25,7 +25,7 @@ class RemindMe(commands.Cog):
         if remind_time is None:
             embed = nextcord.Embed(
                 description="Invalid time format. Use <number>[s|m|h|d].)",
-                color=EMBED_COLOR,
+                color=0xFF0037,
             )
             await ctx.reply(embed=embed, mention_author=False)
             return
@@ -63,7 +63,7 @@ class RemindMe(commands.Cog):
         if remind_time is None:
             embed = nextcord.Embed(
                 description="Invalid time format. Use <number>[s|m|h|d].)",
-                color=EMBED_COLOR,
+                color=0xFF0037,
             )
             await interaction.send(embed=embed, ephemeral=True)
             return
