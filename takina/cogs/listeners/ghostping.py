a263067a324acff4975c5ae936696e2d1039521b
orxngc <orangc@proton.me>
2024-10-26 13:56:54 +0300

    fix(listeners): patch an annoying problem in the ghostping module

--- takina/cogs/listeners/ghostping.py
+++ takina/cogs/listeners/ghostping.py
@@ -13,22 +13,30 @@ class GhostPing(commands.Cog):
         if message.author.bot or not message.mentions:
             return
 
-        for mentioned_user in message.mentions:
-            embed = nextcord.Embed(
-                description=message.content or "*No text content*",
-                color=EMBED_COLOR,
-                timestamp=message.created_at,
-            )
-            embed.set_author(
-                name=f"{message.author.display_name}",
-                icon_url=message.author.avatar.url,
-            )
-            embed.set_footer(text=f"Deleted in #{message.channel.name}")
+        # Consider only the first mentioned user
+        first_mentioned_user = message.mentions[0]
 
-            await message.channel.send(
-                content=f"{mentioned_user.mention}, you were ghost pinged by {message.author.mention}!",
-                embed=embed,
-            )
+        # Ignore if the author mentioned themselves or if the mentioned user is a bot
+        if first_mentioned_user == message.author or first_mentioned_user.bot:
+            return
+
+        # Create an embed for the ghost ping alert
+        embed = nextcord.Embed(
+            description=message.content or "*No text content*",
+            color=EMBED_COLOR,
+            timestamp=message.created_at,
+        )
+        embed.set_author(
+            name=f"{message.author.display_name}",
+            icon_url=message.author.avatar.url,
+        )
+        embed.set_footer(text=f"Deleted in #{message.channel.name}")
+
+        # Send the ghost ping alert message
+        await message.channel.send(
+            content=f"{first_mentioned_user.mention}, you were ghost pinged by {message.author.mention}!",
+            embed=embed,
+        )
 
 
 def setup(bot):
