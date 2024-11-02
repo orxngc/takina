556552561b50ea0f257180c9f65202fa34c097f7
orangci <orangc@proton.me>
2024-11-02 17:12:01 +0300

    feat(listeners): add info commands

--- takina/cogs/listeners/ping_response.py
+++ takina/cogs/listeners/ping_response.py
@@ -4,8 +4,6 @@ from __main__ import EMBED_COLOR
 import os
 from ..libs.oclib import *
 
-PREFIX = os.getenv("PREFIX")
-
 
 class PingResponse(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -13,6 +11,7 @@ class PingResponse(commands.Cog):
         self.stars = 0
         self.forks = 0
         self.bot.loop.create_task(self.fetch_repo_data())
+        self.prefix = os.getenv("PREFIX")
 
     async def fetch_repo_data(self):
         try:
@@ -24,29 +23,45 @@ class PingResponse(commands.Cog):
         except Exception as e:
             print(f"Error fetching repository data: {e}")
 
+    async def construct_info_embed(self):
+        embed = nextcord.Embed(
+            title=f"{await fetch_random_emoji()} Takina",
+            url="https://orangc.xyz/takina",
+            description="Takina is a multipurpose [opensource](https://github.com/orangci/takina) bot written in Python. More information is available in the [website](https://orangc.xyz/takina).",
+            color=EMBED_COLOR,
+        )
+        embed.add_field(name=":sparkles: Prefix", value=self.prefix, inline=True)
+        embed.add_field(name=":star: Stars", value=str(self.stars), inline=True)
+        embed.add_field(
+            name=":alarm_clock: Uptime", value=await uptime_fetcher(), inline=True
+        )
+        orangc = await self.bot.fetch_user(961063229168164864)
+        embed.set_author(
+            name="orangc",
+            url="https://orangc.xyz",
+            icon_url=orangc.avatar.url
+            or "https://cdn.discordapp.com/avatars/961063229168164864/4bfbf378514a9dcc7a619b5ce5e7e57c.webp",
+        )
+        return embed
+
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if self.bot.user.mentioned_in(message) and not message.author.bot:
             if message.content.strip() == message.guild.me.mention:
-                emoji = await fetch_random_emoji()
-                embed = nextcord.Embed(
-                    title=f"{emoji} Takina",
-                    url="https://orangc.xyz/takina",
-                    description="Takina is a multipurpose [opensource](https://github.com/orangci/takina) bot written in Python. More information is available in the [website](https://orangc.xyz/takina).",
-                    color=EMBED_COLOR,
+                await message.reply(
+                    embed=await construct_info_embed(), mention_author=False
                 )
-                uptime = await uptime_fetcher()
-                embed.add_field(name=":sparkles: Prefix", value=PREFIX, inline=True)
-                embed.add_field(name=":star: Stars", value=str(self.stars), inline=True)
-                embed.add_field(name=":alarm_clock: Uptime", value=uptime, inline=True)
-                orangc = await self.bot.fetch_user(961063229168164864)
-                embed.set_author(
-                    name="orangc",
-                    url="https://orangc.xyz",
-                    icon_url=orangc.avatar.url
-                    or "https://cdn.discordapp.com/avatars/961063229168164864/4bfbf378514a9dcc7a619b5ce5e7e57c.webp",
-                )
-                await message.reply(embed=embed, mention_author=False)
+
+    @commands.command(
+        name="info", description="Information about the bot.", help="Usage: `info`."
+    )
+    @commands.cooldown(1, 1, commands.BucketType.user)
+    async def info(self, ctx: commands.Context):
+        await ctx.reply(embed=await construct_info_embed(), mention_author=False)
+
+    @nextcord.slash_command(name="info", description="Information about the bot.")
+    async def slash_info(self, interaction: nextcord.Interaction):
+        await interaction.send(embed=await construct_info_embed(), ephemeral=True)
 
 
 def setup(bot: commands.Bot):
