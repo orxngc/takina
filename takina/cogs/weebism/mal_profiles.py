51f052a9ccaa94f274442a61b9b22e00639eace5
orangci <orangc@proton.me>
2024-12-09 14:31:21 +0300

    refactor(weebism): make `mal` display days watched/read

--- takina/cogs/weebism/mal_profiles.py
+++ takina/cogs/weebism/mal_profiles.py
@@ -26,30 +26,51 @@ class MAL_Profiles(commands.Cog):
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def mal(self, ctx: commands.Context, *, username: str):
         try:
-            profile_url = f"https://api.jikan.moe/v4/users/{username}"
-            profile_data = await request(profile_url)
+            profile_data = await request(f"https://api.jikan.moe/v4/users/{username}")
 
             if not profile_data or not profile_data.get("data"):
-                embed = nextcord.Embed(title="User not found.", color=0xFF0037)
-                await ctx.reply(embed=embed, mention_author=False)
+                embed = nextcord.Embed(title="User not found.", color=EMBED_COLOR)
+                await ctx.response.send_message(embed=embed, ephemeral=True)
                 return
 
             user = profile_data["data"]
+
+            mal_id = user.get("mal_id")
             profile_url = user.get("url")
             profile_pic = user.get("images", {}).get("jpg", {}).get("image_url", "")
             gender = user.get("gender") or "Not Specified"
             last_online = format_date_long(user.get("last_online"))
             joined = format_date(user.get("joined"))
             location = user.get("location") or "Not Specified"
-            mal_id = user.get("mal_id")
             anime_list_url = f"https://myanimelist.net/animelist/{username}"
             manga_list_url = f"https://myanimelist.net/mangalist/{username}"
 
-            stats_url = f"https://api.jikan.moe/v4/users/{username}/statistics"
-            stats_data = await request(stats_url)
-
-            anime_mean = stats_data.get("data", {}).get("anime", {}).get("mean_score")
-            manga_mean = stats_data.get("data", {}).get("manga", {}).get("mean_score")
+            # stats
+            profile_stats = await request(f"https://api.jikan.moe/v4/users/{username}/statistics")
+            
+            anime_stats = profile_stats["data"].get("anime")
+            days_watched = f"**{str(anime_stats.get("days_watched"))}**"
+            anime_mean = f"**{str(anime_stats.get("mean_score"))}**"
+            # watching = f"**{str(anime_stats.get("watching"))}**"
+            # anime_completed = f"**{str(anime_stats.get("completed"))}**"
+            # anime_on_hold = f"**{str(anime_stats.get("on_hold"))}**"
+            # anime_dropped = f"**{str(anime_stats.get("dropped"))}**"
+            # ptw = f"**{str(anime_stats.get("plan_to_watch"))}**"
+            # anime_total_entries = f"**{str(anime_stats.get("total_entries"))}**"
+            # rewatched = f"**{str(anime_stats.get("rewatched"))}**"
+            # anime_episodes = f"**{str(anime_stats.get("episodes_watched"))}**"
+            
+            manga_stats = profile_stats["data"].get("manga")
+            days_read = f"**{str(manga_stats.get("days_read"))}**"
+            manga_mean = f"**{str(manga_stats.get("mean_score"))}**"
+            # reading = f"**{str(manga_stats.get("reading"))}**"
+            # manga_completed = f"**{str(manga_stats.get("completed"))}**"
+            # manga_on_hold = f"**{str(manga_stats.get("on_hold"))}**"
+            # manga_dropped = f"**{str(manga_stats.get("dropped"))}**"
+            # ptr = f"**{str(manga_stats.get("plan_to_read"))}**"
+            # manga_total_entries = f"**{str(manga_stats.get("total_entries"))}**"
+            # reread = f"**{str(manga_stats.get("reread"))}**"
+            # chapters = f"**{str(manga_stats.get("chapters_read"))}**"
 
             embed = nextcord.Embed(
                 title=f"{username}'s Profile",
@@ -64,8 +85,8 @@ class MAL_Profiles(commands.Cog):
             embed.description += f"\n> **Last Online**: {last_online}"
             embed.description += f"\n> **Joined**: {joined}"
             embed.description += f"\n> **Location**: {location}"
-            embed.description += f"\n> **Anime Mean**: {str(anime_mean)}"
-            embed.description += f"\n> **Manga Mean**: {str(manga_mean)}"
+            embed.description += f"\n> **Anime:** {days_watched} days watched with a mean score of {anime_mean}."
+            embed.description += f"\n> **Manga:** {days_read} days read with a mean score of {manga_mean}."
             embed.set_footer(text=str(mal_id))
 
             if profile_pic:
@@ -88,8 +109,7 @@ class MAL_Profiles(commands.Cog):
         ),
     ):
         try:
-            profile_url = f"https://api.jikan.moe/v4/users/{username}"
-            profile_data = await request(profile_url)
+            profile_data = await request(f"https://api.jikan.moe/v4/users/{username}")
 
             if not profile_data or not profile_data.get("data"):
                 embed = nextcord.Embed(title="User not found.", color=EMBED_COLOR)
@@ -97,21 +117,43 @@ class MAL_Profiles(commands.Cog):
                 return
 
             user = profile_data["data"]
+
+            mal_id = user.get("mal_id")
             profile_url = user.get("url")
             profile_pic = user.get("images", {}).get("jpg", {}).get("image_url", "")
             gender = user.get("gender") or "Not Specified"
             last_online = format_date_long(user.get("last_online"))
             joined = format_date(user.get("joined"))
             location = user.get("location") or "Not Specified"
-            mal_id = user.get("mal_id")
             anime_list_url = f"https://myanimelist.net/animelist/{username}"
             manga_list_url = f"https://myanimelist.net/mangalist/{username}"
 
-            stats_url = f"https://api.jikan.moe/v4/users/{username}/statistics"
-            stats_data = await request(stats_url)
-
-            anime_mean = stats_data.get("data", {}).get("anime", {}).get("mean_score")
-            manga_mean = stats_data.get("data", {}).get("manga", {}).get("mean_score")
+            # stats
+            profile_stats = await request(f"https://api.jikan.moe/v4/users/{username}/statistics")
+            
+            anime_stats = profile_stats["data"].get("anime")
+            days_watched = f"**{str(anime_stats.get("days_watched"))}**"
+            anime_mean = f"**{str(anime_stats.get("mean_score"))}**"
+            # watching = f"**{str(anime_stats.get("watching"))}**"
+            # anime_completed = f"**{str(anime_stats.get("completed"))}**"
+            # anime_on_hold = f"**{str(anime_stats.get("on_hold"))}**"
+            # anime_dropped = f"**{str(anime_stats.get("dropped"))}**"
+            # ptw = f"**{str(anime_stats.get("plan_to_watch"))}**"
+            # anime_total_entries = f"**{str(anime_stats.get("total_entries"))}**"
+            # rewatched = f"**{str(anime_stats.get("rewatched"))}**"
+            # anime_episodes = f"**{str(anime_stats.get("episodes_watched"))}**"
+            
+            manga_stats = profile_stats["data"].get("manga")
+            days_read = f"**{str(manga_stats.get("days_read"))}**"
+            manga_mean = f"**{str(manga_stats.get("mean_score"))}**"
+            # reading = f"**{str(manga_stats.get("reading"))}**"
+            # manga_completed = f"**{str(manga_stats.get("completed"))}**"
+            # manga_on_hold = f"**{str(manga_stats.get("on_hold"))}**"
+            # manga_dropped = f"**{str(manga_stats.get("dropped"))}**"
+            # ptr = f"**{str(manga_stats.get("plan_to_read"))}**"
+            # manga_total_entries = f"**{str(manga_stats.get("total_entries"))}**"
+            # reread = f"**{str(manga_stats.get("reread"))}**"
+            # chapters = f"**{str(manga_stats.get("chapters_read"))}**"
 
             embed = nextcord.Embed(
                 title=f"{username}'s Profile",
@@ -126,8 +168,8 @@ class MAL_Profiles(commands.Cog):
             embed.description += f"\n> **Last Online**: {last_online}"
             embed.description += f"\n> **Joined**: {joined}"
             embed.description += f"\n> **Location**: {location}"
-            embed.description += f"\n> **Anime Mean**: {str(anime_mean)}"
-            embed.description += f"\n> **Manga Mean**: {str(manga_mean)}"
+            embed.description += f"\n> **Anime:** {days_watched} days watched with a mean score of {anime_mean}."
+            embed.description += f"\n> **Manga:** {days_read} days read with a mean score of {manga_mean}."
             embed.set_footer(text=str(mal_id))
 
             if profile_pic:
